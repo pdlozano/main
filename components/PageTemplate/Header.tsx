dd4567f69221568c656d8d0baa047afe480d0560
David Lozano <hello@davidlozano.me>
2022-02-04 15:06:05 +0800

    Create Beginner Hamburger Menu

--- components/PageTemplate/Header.tsx
+++ components/PageTemplate/Header.tsx
@@ -1,30 +1,37 @@
 import {useState} from "react";
-import {Menu} from "react-feather";
+import {MoreVertical, X} from "react-feather";
 import {Link} from ".";
 
 function Header() {
-    const [item, setItem] = useState<boolean>(false);
+    const [item, setItem] = useState<boolean>(true);
 
     return (
         <header className="flex items-center justify-between mx-5 bg-gray px-5 py-1 rounded mt-4 drop-shadow">
-            <p className="text-xl font-bold font-body">David Lozano</p>
+            <p className="text-xl font-bold font-body"><a href="/">David Lozano</a></p>
 
-            <div>
-                <button aria-label="Menu" onClick={(event) => {
+            <nav>
+                <button aria-label="Open Menu" onClick={(event) => {
                     event.preventDefault();
                     setItem(!item);
                 }}>
-                    <Menu />
+                    <MoreVertical/>
                 </button>
 
-                {item ?
-                    <ul className="">
-                        <Link href="/instagram">Instagram</Link>
-                    </ul>
-                    : ""
-                }
-            </div>
+                {item ? "" : (
+                    <div className="flex flex-col items-end py-5 px-10 fixed h-screen bg-gray inset-y-0 right-0 w-full md:w-4/12 -mx-5 -mt-4">
+                        <button aria-label="Close Menu" onClick={(event) => {
+                            event.preventDefault();
+                            setItem(!item);
+                        }}>
+                            <X/>
+                        </button>
 
+                        <ul className="w-full">
+                            <Link href="/instagram">Instagram</Link>
+                        </ul>
+                    </div>
+                )}
+            </nav>
         </header>
     );
 }
