15756b08e45e634f1363effc9363eb63c70543ef
David Lozano <hello@davidlozano.me>
2022-02-04 14:54:33 +0800

    Change message
    I don't feel like pursuing a license anymore

--- components/MainPageHead.tsx
+++ components/MainPageHead.tsx
@@ -5,7 +5,7 @@ function MainPageHead() {
         <div className="min-h-screen flex items-center justify-center">
             <div className="w-10/12 md:w-8/12">
                 <h1>Hi! I&apos;m David.</h1>
-                <p>Web Developer and Future Electrical Engineer.</p>
+                <p>Software Developer. Electrical Engineering Student.</p>
 
                 <Icons/>
             </div>
