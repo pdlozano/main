0f6083e328cf35dd51c749d5fff5ce13ea756244
David Lozano <hello@davidlozano.me>
2022-02-16 17:34:07 +0800

    Improve styling of Main Page Head
    This improves how the introduction to the main site looks like.
    The major changes are:
    
    - The Call-to-action and Icons are now in one row on desktop.
      Previously, the Icons went first, but it caused a weird look.
    - The "For Hire" and "Not For Hire" does not use state anymore.
      In fact, it is temporarily removed for now but the code for
      it is ready to use.

--- components/IndexPage/MainPageHead.tsx
+++ components/IndexPage/MainPageHead.tsx
@@ -1,19 +1,26 @@
 import Icons from "../Icons";
-import {useState} from "react";
+
+function ForHire({hire}: { hire: boolean }) {
+    return null;
+    // return hire ? <p className="text-green">For Hire</p> : <p>Not For Hire</p>;
+}
 
 function MainPageHead() {
-    const [forHire, setForHire] = useState(false);
-
     return (
-        <div className="mt-20 md:mt-36 flex items-center justify-center">
+        <div className="my-10 md:my-24 flex items-center justify-center">
             <div className="w-10/12 md:w-8/12">
                 <h1>Hi! Call me David.</h1>
-                <p>{forHire ? "For Hire" : "Not For Hire"}</p>
+                <ForHire hire={false}/>
                 <p>I am a web developer and electrical engineering student.</p>
 
-                <Icons/>
+                <nav className="flex flex-col md:flex-row md:items-center">
+                    <button className="cta mr-2">Read my Blog</button>
 
-                <button className="cta">Read my Blog</button>
+                    <div className="mt-5 mx-auto w-100 md:m-0">
+                        <Icons/>
+                    </div>
+                </nav>
+
             </div>
         </div>
     )
