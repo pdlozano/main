dab995afb110b6a40e6b152f0144280ddbf5ef9f
David Lozano <hello@davidlozano.me>
2022-02-18 11:12:36 +0800

    Add more portfolio items
    I added three more items to make it more complete.

--- components/PortfolioPage/Items.tsx
+++ components/PortfolioPage/Items.tsx
@@ -1,6 +1,9 @@
 import PortfolioItem from "./PortfolioItem";
 import Pokemon from "../../public/portfolio/pokemon.jpg";
 import Advocacy from "../../public/portfolio/gay-blood.jpg";
+import Omega from "../../public/portfolio/omega.jpg";
+import Sketch from "../../public/portfolio/sketch.jpg";
+import Trivia from "../../public/portfolio/trivia.jpg";
 
 function Items() {
     return (
@@ -26,6 +29,39 @@ function Items() {
                 This is originally a school project that I just decided to keep.
                 The advocacy is something I firmly believe in and would help our
                 LGBT brothers and sisters save lives.
+            </PortfolioItem>
+
+            <PortfolioItem
+                title="Omega Blog"
+                link="https://github.com/pdlozano/omega"
+                website="https://omega.davidlozano.me"
+                image={Omega}
+            >
+                A health blog I used to write. While I am not a doctor, I aimed
+                to be that friend you talk to when you have a health problem.
+                However, I eventually stopped since I believe I could do more
+                harm than good.
+            </PortfolioItem>
+
+            <PortfolioItem
+                title="Sketching App"
+                link="https://github.com/pdlozano/sketch-an-etch"
+                website="https://sketch.davidlozano.me"
+                image={Sketch}
+            >
+                One of my first projects that I was genuinely proud of. It is a
+                simple app that lets you sketch when you hover over the items.
+            </PortfolioItem>
+
+            <PortfolioItem
+                title="Trivia App"
+                link="https://github.com/pdlozano/trivia"
+                website="https://trivia.davidlozano.me"
+                image={Trivia}
+            >
+                A small app that quizzes you on various topics. I am surprised
+                how niche some of the questions are (the data comes from another
+                service). Try it out and see how much you get.
             </PortfolioItem>
         </div>
     );
