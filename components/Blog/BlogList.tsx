4a7d4ac1d4a62b12878667d4ebe36b464e3078d6
David Lozano <hello@davidlozano.me>
2022-02-18 10:41:20 +0800

    Improve the style of BlogList
    This improves how posts from BlogList look. Instead of a
    simple list, it now looks more like the ones I had from the
    Omega blog.

--- components/Blog/BlogList.tsx
+++ components/Blog/BlogList.tsx
@@ -10,9 +10,12 @@ function BlogList(props: { data: BlogItemData[] }) {
     return (
         <ul>
             {props.data.map((item) => (
-                <li key={item.title}>
+                <li key={item.title} className="list-none">
                     <Link href={"/blog/" + item.url}>
-                        {`${item.title} (Published on ${item.date})`}
+                        <a className="bg-gray rounded px-5 py-4 block border-2 border-gray hover:border-main-600 hover:no-underline focus:border-main-300">
+                            <h2 className="text-2xl">{item.title}</h2>
+                            <p className="text-sm">Published on {item.date}</p>
+                        </a>
                     </Link>
                 </li>
             ))}
