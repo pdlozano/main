f7b1e681b5dec537cc213a7bc12cdbf82d39f643
David Lozano <hello@davidlozano.me>
2022-02-18 10:33:49 +0800

    Separate Markdown Component from Blog Post
    This commit does two things:
    - Separates the Markdown Component from Blog Post to
      its own separate file. This is to ensure that it can
      be used again in case it will be needed in pages.
    - Dynamically imports the new Markdown component into
      Blog Post. This will make sure that the title and
      date appear first in the new page and makes the
      site feels faster.

--- components/Blog/BlogPost.tsx
+++ components/Blog/BlogPost.tsx
@@ -1,6 +1,6 @@
-import ReactMarkdown from "react-markdown";
-import { Prism } from "react-syntax-highlighter";
-import codeStyle from "../customCodeStyle";
+import dynamic from "next/dynamic";
+
+const Markdown = dynamic(() => import("./Markdown"));
 
 type PostData = {
     title: string;
@@ -17,31 +17,7 @@ function BlogPost(props: PostData) {
             </header>
 
             <main>
-                <ReactMarkdown
-                    components={{
-                        code({ node, inline, className, children, ...props }) {
-                            const match = /language-(\w+)/.exec(
-                                className || ""
-                            );
-                            return !inline && match ? (
-                                <Prism
-                                    style={codeStyle}
-                                    language={match[1]}
-                                    PreTag="div"
-                                    {...props}
-                                >
-                                    {String(children).replace(/\n$/, "")}
-                                </Prism>
-                            ) : (
-                                <code className={className} {...props}>
-                                    {children}
-                                </code>
-                            );
-                        },
-                    }}
-                >
-                    {props.content}
-                </ReactMarkdown>
+                <Markdown>{props.content}</Markdown>
             </main>
         </>
     );
