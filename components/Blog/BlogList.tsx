c7d7099dfbb087f9dc69085bc41f115539482106
David Lozano <hello@davidlozano.me>
2022-02-18 10:55:37 +0800

    Improve styling of Portfolio Items
    This improves how the portfolio items look. Since I found it
    is mostly similar to how the BlogList items look, I took out
    the component and used it again.
    
    This also adds some images from webdev.davidlozano.me

--- components/Blog/BlogList.tsx
+++ components/Blog/BlogList.tsx
@@ -12,7 +12,7 @@ function BlogList(props: { data: BlogItemData[] }) {
             {props.data.map((item) => (
                 <li key={item.title} className="list-none">
                     <Link href={"/blog/" + item.url}>
-                        <a className="bg-gray rounded px-5 py-4 block border-2 border-gray hover:border-main-600 hover:no-underline focus:border-main-300">
+                        <a className="link-box">
                             <h2 className="text-2xl">{item.title}</h2>
                             <p className="text-sm">Published on {item.date}</p>
                         </a>
--- components/PortfolioPage/Items.tsx
+++ components/PortfolioPage/Items.tsx
@@ -1,13 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import Pokemon from "../../public/portfolio/pokemon.jpg";
+
 type ItemProps = {
     title: string;
+    link: string;
+    children: string;
+    image: StaticImageData;
     tech: ("SQL" | "JavaScript" | "Python")[];
 };
 
 function Item(props: ItemProps) {
     return (
-        <div>
-            <h2>{props.title}</h2>
-        </div>
+        <Link href={props.link}>
+            <a className="link-box grid md:grid-cols-5 gap-3">
+                <div className="md:col-span-2">
+                    <Image src={props.image} alt={`Image of ${props.title}`} />
+                </div>
+
+                <div className="md:col-span-3">
+                    <h2>{props.title}</h2>
+                    <p className="text-white">{props.children}</p>
+                </div>
+            </a>
+        </Link>
     );
 }
 
@@ -16,8 +32,15 @@ function Items() {
         <div>
             <Item
                 title="Pokemon Team Stats and Coverage App"
+                link="https://github.com/pdlozano/pokemon"
+                image={Pokemon}
                 tech={["JavaScript"]}
-            />
+            >
+                I play Pokemon in my free time and one of the things I have a
+                hard time doing is making sure my team has proper coverage and
+                is strong enough to defeat teams. Thus, this was born out of
+                personal need.
+            </Item>
         </div>
     );
 }
