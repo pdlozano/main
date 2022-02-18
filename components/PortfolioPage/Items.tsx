98c89ed4a87adf4c0cacf1ba6c554638372a5172
David Lozano <hello@davidlozano.me>
2022-02-18 11:04:21 +0800

    Add another Item and let people see the real thing
    I just realized I only link to the GitHub page on my portfolio.
    This commit adds a button to link people to the real thing.

--- components/PortfolioPage/Items.tsx
+++ components/PortfolioPage/Items.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import Pokemon from "../../public/portfolio/pokemon.jpg";
+import Advocacy from "../../public/portfolio/gay-blood.jpg";
 
 type ItemProps = {
     title: string;
     link: string;
     children: string;
+    website: string;
     image: StaticImageData;
 };
 
@@ -19,6 +21,13 @@ function Item(props: ItemProps) {
 
                 <div className="md:col-span-3">
                     <h2>{props.title}</h2>
+
+                    <Link href={props.website}>
+                        <a className="cta bg-gray inline-block mt-3 w-full text-center md:w-auto">
+                            See it in action here
+                        </a>
+                    </Link>
+
                     <p className="text-white">{props.children}</p>
                 </div>
             </a>
@@ -32,12 +41,24 @@ function Items() {
             <Item
                 title="Pokemon Team Stats and Coverage App"
                 link="https://github.com/pdlozano/pokemon"
+                website="https://pokemon.davidlozano.me"
                 image={Pokemon}
             >
                 I play Pokemon in my free time and one of the things I have a
                 hard time doing is making sure my team has proper coverage and
                 is strong enough to defeat teams. Thus, this was born out of
                 personal need.
+            </Item>
+
+            <Item
+                title="Advocacy Page for Allowing Gays to Donate"
+                link="https://github.com/pdlozano/advocacy-school"
+                website="https://gay-blood.davidlozano.me"
+                image={Advocacy}
+            >
+                This is originally a school project that I just decided to keep.
+                The advocacy is something I firmly believe in and would help our
+                LGBT brothers and sisters save lives.
             </Item>
         </div>
     );
