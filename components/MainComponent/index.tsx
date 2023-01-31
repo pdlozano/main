c166f0a961f43c1ab86a15a1d9fabaf8e16f02c7
David Lozano <hello@davidlozano.me>
2023-01-31 21:17:05 +0800

    Add website description

--- components/MainComponent/index.tsx
+++ components/MainComponent/index.tsx
@@ -8,6 +8,8 @@ type MainProps = {
     description?: string;
 };
 
+const defaultDescription = "Call me David. I live in Cebu, PH and am currently a college student. I like everything data and I study it a lot in my free time.";
+
 function MainComponent(props: MainProps) {
     return (
         <div className="flex flex-col min-h-screen">
@@ -18,7 +20,7 @@ function MainComponent(props: MainProps) {
                     content="width=device-width, initial-scale=1.0"
                 />
                 <meta charSet="UTF-8" />
-                <meta name="description" content={props.description ? props.description : ""} />
+                <meta name="description" content={props.description ? props.description : defaultDescription} />
             </Head>
 
             <Header />
